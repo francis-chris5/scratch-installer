85de68ebcb3d57db4cb65cbd7e628b1c3c034ea9
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2023-10-10 19:52:16 -0400

    Add files via upload

---
+++ identity_theft.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+REM check to confirm all resources are present before running our software
+REM just make sure it is safe to run our software
+
+java -jar identity_theft.jar
+
+PAUSE
---
+++ identity_theft.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## check to confirm all resources are present before running our software
+## just make sure it is safe to run our software
+
+java -jar identity_theft.jar
+
